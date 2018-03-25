ad4b28d4063871bf852c991ff779fdc4988ab397
Duminica Octavian <duminicaoctavian@192-168-0-101.rdsnet.ro>
2018-03-25 19:08:47 +0300

    Initial Commit

---
+++ TutorialsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TutorialsApp
+//
+//  Created by Duminica Octavian on 25/03/2018.
+//  Copyright © 2018 Duminica Octavian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
